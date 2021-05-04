971994e30739ae23bba9175a10d16463224f86e3
Sachin Sharma <krisa1826@gmail.com>
2021-05-04 11:35:53 +0530

    heroku deploy
    v1

--- api/settings.py
+++ api/settings.py
@@ -111,10 +111,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS =(
-    os.path.join(BASE_DIR, 'assets'),
-)
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 #Media SetUp
