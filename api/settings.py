9732fc367428dfd550f0498b97916c3fed036348
Sachin Sharma <krisa1826@gmail.com>
2021-05-21 16:23:29 +0530

    updated settings

--- api/settings.py
+++ api/settings.py
@@ -1,16 +1,18 @@
 import os
 from pathlib import Path
+from decouple import config
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v%x1r9=66-)gr(gad!wmg(cbu92_^wyylhut*u%36clme8j0#4'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['65.0.180.250']
 
 
 # Application definition
@@ -108,16 +110,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = ( 
+    '/home/ubuntu/api/assets',
+)
+
+STATIC_ROOT = '/home/ubuntu/api/static/'
 
 
 #Media SetUp
 
 MEDIA_URL = '/media/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/home/ubuntu/api/media'
 
 
 # Default primary key field type
